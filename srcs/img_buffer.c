b766cfc8ebc137feacdc31ae9dae5df588abc610
Seunghyun Han <seunghan@c6r4s4.42seoul.kr>
2024-08-01 16:21:14 +0900

    texture print flip problem fix commit#1

--- srcs/img_buffer.c
+++ srcs/img_buffer.c
@@ -6,7 +6,7 @@
 /*   By: jeshin <jeshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 11:18:17 by jeshin            #+#    #+#             */
-/*   Updated: 2024/07/30 13:28:10 by jeshin           ###   ########.fr       */
+/*   Updated: 2024/08/01 16:20:14 by seunghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,9 @@ void	mapping_to_texture(t_ray *ray, t_player *player, \
 		ray->wall_x = player->pos_x + ray->perp_wall_dist * ray->dir_x;
 	ray->wall_x -= floor(ray->wall_x);
 	tex_info->tex_x = (int)(ray->wall_x * (double)tex->width);
-	if (ray->side == 0 && ray->dir_x > 0)
+	if (ray->side == 0 && ray->dir_x >= 0)
 		tex_info->tex_x = tex->width - tex_info->tex_x - 1;
-	if (ray->side == 1 && ray->dir_x < 0)
+	if (ray->side == 1 && ray->dir_y < 0)
 		tex_info->tex_x = tex->width - tex_info->tex_x - 1;
 	tex_info->step = 1.0 * tex->height / ray->line_height;
 	tex_info->pos = (ray->draw_start - SCREENHEIGHT / 2 + ray->line_height / 2) \
