84bf23ea7ff28d2b95cdbcb02cecff39c653dd3b
Seunghyun Han <seunghan@c6r4s4.42seoul.kr>
2024-08-04 19:08:09 +0900

    left right oper fix commit #1

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: jeshin <jeshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 19:08:40 by jeshin            #+#    #+#             */
-/*   Updated: 2024/07/30 19:02:30 by jeshin           ###   ########.fr       */
+/*   Updated: 2024/08/04 19:04:14 by seunghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ int	move_left(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player->pos_x - data->player->dir_y * MOVESPEED;
-	new_y = data->player->pos_y + data->player->dir_x * MOVESPEED;
+	new_x = data->player->pos_x + data->player->dir_y * MOVESPEED;
+	new_y = data->player->pos_y - data->player->dir_x * MOVESPEED;
 	check_validate(&new_y, &new_x, data->map_info->map, data->player);
 	data->player->pos_x = new_x;
 	data->player->pos_y = new_y;
@@ -56,8 +56,8 @@ int	move_right(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player->pos_x + data->player->dir_y * MOVESPEED;
-	new_y = data->player->pos_y - data->player->dir_x * MOVESPEED;
+	new_x = data->player->pos_x - data->player->dir_y * MOVESPEED;
+	new_y = data->player->pos_y + data->player->dir_x * MOVESPEED;
 	check_validate(&new_y, &new_x, data->map_info->map, data->player);
 	data->player->pos_x = new_x;
 	data->player->pos_y = new_y;
